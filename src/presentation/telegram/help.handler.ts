d86ae3d7482fe9b9c497fce9456c31c680d3496b
Igor Kamyshev <igor@kamyshev.me>
2021-06-30 16:15:19 +0700

    add basic Help

--- src/presentation/telegram/help.handler.ts
+++ src/presentation/telegram/help.handler.ts
@@ -3,13 +3,26 @@ import { Context, TelegramActionHandler } from 'nest-telegram';
 
 @Injectable()
 export class HelpHandler {
+  private HELP = [
+    "Send me any YouTube-video, and I'll create a personal RSS feed for you, download the video, convert it to audio and put in the feed.",
+    'Personal RSS-feed can be used in any podcast-application ([Apple Podcasts](https://support.patreon.com/hc/en-us/articles/115000877506-Add-my-private-RSS-feed-to-the-Apple-Podcast-app), Pocket Casts, Overcast, etc.).',
+  ];
+
   @TelegramActionHandler({ onStart: true })
   async start(ctx: Context) {
-    await ctx.reply('Hello!');
+    const message = this.formatMessage(["I'm YT Listen bot 👋", ...this.HELP]);
+
+    await ctx.reply(message, { parse_mode: 'Markdown' });
   }
 
   @TelegramActionHandler({ command: '/help' })
   async help(ctx: Context) {
-    await ctx.reply('Help!');
+    const message = this.formatMessage(this.HELP);
+
+    await ctx.reply(message, { parse_mode: 'Markdown' });
+  }
+
+  private formatMessage(message: string[]): string {
+    return message.join('\n\n');
   }
 }
